30e2f154a4e62f5ab8f9f1184712eded1da978e0
w.stoettinger <wolfgang.stoettinger@gmail.com>
2015-07-27 19:28:15 +0200

    minification

---
+++ web/js/perfmatters.min.js
@@ -0,0 +1,1 @@
+function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading,a=a.domComplete-a.domLoading;document.getElementById("crp-stats").textContent="DCL: "+b+"ms, onload: "+a+"ms"}window.addEventListener("load",function(a){logCRP()});
